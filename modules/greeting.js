260bbc55a09aa8649620d1936bd0a1f72bc6987a
Lev Ostapenko <012938576a@gmail.com>
2023-04-20 16:34:08 +0300

    changed greeting.js module: now it present var and function to get greeting message

--- modules/greeting.js
+++ modules/greeting.js
@@ -1,5 +1,16 @@
-//Модуль для работы с системой
-const os = require("os"); 
-let username = os.userInfo().username;
+
+let currentDate = new Date();
+module.exports.date = currentDate;
+
+module.exports.getMessage = function(name) {
+    let hour = currentDate.getHours();
+
+    if (hour < 12) 
+        return "Good morning, " + name;
+    else if (hour > 16)
+        return "Good evening, " + name;
+    else 
+        return "Good day, " + name; 
+}
 
 console.log(`Hello, ${username}`)
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require("http");
 
+//Модуль для работы с системой
+const os = require("os"); 
+let username = os.userInfo().username;
+
 const greeting = require("./modules/greeting")
 
+var helloMsg = greeting.getMessage(username)
+
 http.createServer(function(request, response) {
-    response.end("Hello Nodejs!");
+    response.end(helloMsg);
 }).listen(3000, "127.0.0.1", function() {
     console.log(`Server is listening on 3000 port!`)
 });
