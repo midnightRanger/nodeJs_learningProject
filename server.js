f23071be14714a1a85f04e81155351c70c6e0436
Lev Ostapenko <012938576a@gmail.com>
2023-05-16 18:27:52 +0300

    added package express

--- server.js
+++ server.js
@@ -11,11 +11,20 @@ var helloMsg = greeting.getMessage(username)
 //User Constuctor
 const User = require("./models/user")
 
+//express 
+const express = require("express")
+const app = express();
+
+app.get("/", function(request, response){
+    response.end("Hello from Express!")
+});
+
 let ivan = new User("Ivan", 14);
 ivan.displayInfo();
 
-http.createServer(function(request, response) {
-    response.end(helloMsg);
-}).listen(3000, "127.0.0.1", function() {
-    console.log(`Server is listening on 3000 port!`)
-});
+// http.createServer(function(request, response) {
+//     response.end(helloMsg);
+// }).listen(3000, "127.0.0.1", function() {
+//     console.log(`Server is listening on 3000 port!`)
+// });
+app.listen(3000)
